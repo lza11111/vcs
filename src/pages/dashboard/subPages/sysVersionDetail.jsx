fe10d27b093720c6ab9667a7338b3020208be663
iris19990802 <liuyuex@sas.upenn.edu>
2020-05-13 15:08:03 +0800

    333

--- src/pages/dashboard/subPages/sysVersionDetail.jsx
+++ src/pages/dashboard/subPages/sysVersionDetail.jsx
@@ -7,6 +7,59 @@ function sysVersionDetail(props) {
   const { sysDetailData } = props;
   const { OSDetail , saData } = sysDetailData;
 
+  const modelMap = saData.rows.reduce(
+    (prev, curr) => (
+    (prev[curr.moduleId] = prev[curr.moduleId] 
+      ? prev[curr.moduleId] + 1 : 1) && prev
+  ), {});
+  console.log(modelMap);
+
+  const mergeModelColumn = (text, record, index) => {
+    if (index > 0) {
+      return {
+        children: text,
+        props: {
+          rowSpan: record.moduleId === saData.rows[index - 1].moduleId ?
+            0 : modelMap[record.moduleId]
+        }
+      }
+    }
+    return text;
+  }
+
+  const column = [{
+    title: '模块名',
+    dataIndex: 'moduleName',
+    render: mergeModelColumn,
+  }, {
+    title: '模块版本',
+    dataIndex: 'moduleVersion',
+    render: mergeModelColumn,
+  }, {
+    title: '模块进度',
+    dataIndex: 'moduleProgress',
+    render: mergeModelColumn,
+  } , {
+    title: '服务名',
+    dataIndex: 'serviceName',
+  } , {
+    title: '服务进度',
+    dataIndex: 'serviceProgress',
+  } , {
+    title: '功能',
+    dataIndex: 'function',
+  } , {
+    title: '代码分支',
+    dataIndex: 'codeBranch',
+  } , {
+    title: '计划tag',
+    dataIndex: 'serviceTag',
+  } , {
+    title: '负责人',
+    dataIndex: 'manager',
+  }]
+
+
   return (
     <div className={styles.sysVersion}>
       <Row>
@@ -27,7 +80,12 @@ function sysVersionDetail(props) {
           onChange: (page,defaultPageSize) => 
             dispatch({ 
               type: 'sysDetail/queryServiceListByOsVersion', 
-              payload: {page,defaultPageSize} 
+              payload: {
+                osVersion: OSDetail.osVersion,
+                osName: OSDetail.osName,
+                page: page,
+                pageSize: defaultPageSize
+              } 
             }) 
           }}
       />
