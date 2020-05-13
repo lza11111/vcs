36a4e8c6ebb2c848b880ae6f9395ad9ba394dda9
iris19990802 <liuyuex@sas.upenn.edu>
2020-05-13 15:30:34 +0800

    444

--- src/pages/dashboard/subPages/appVersion.jsx
+++ src/pages/dashboard/subPages/appVersion.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 function appVersion() {
+
   return (
-    <div>
-      appVersion
+    <div className={styles.sysVersion}>
+      <Row span={24}>
+        <Col span={3}><b>服务名：</b>{wipVersion.osVersion}</Col>
+        <Col span={10}><Progress percent={wipVersion.osProgress * 100} /></Col>
+      </Row>
     </div>)
+
 };
 
 export default appVersion;
